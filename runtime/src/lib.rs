4871bbc99777566613df672e54b95104fe019995
Devon Hockley <devon@hockley.ca>
2022-01-14 22:46:29 -0700

    added other methods

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -20,13 +20,5 @@ pub fn write_bytemuck_to_wasm_memory<T : bytemuck::Pod >(data: T, memory: &Memor
 }
 
 pub fn write_bytes_to_wasm_memory(bytes: &[u8], memory: &Memory, ptr: usize, len: usize){
-    let mem_array: &mut [u8];
-    unsafe {
-        mem_array = memory.data_unchecked_mut();
-        for i in 0..len {
-            // iterate over the serialized struct, copying it to the memory of the target module,
-            // using the ptr provided by caller
-            mem_array[ptr + i as usize] = bytes[i as usize];
-        }
-    }
+    modularis::write_bytes_to_wasm_memory(bytes,memory,ptr,len)
 }
