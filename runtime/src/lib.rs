d8800e70642894d7123059f30a860bc55363aa4e
Devon Hockley <devon@hockley.ca>
2022-03-28 13:13:24 -0600

    migrated to shared lib

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -9,14 +9,11 @@ mod tests {
 use wasmer::Memory;
 
 pub fn write_bincode_to_wasm_memory<T : serde::Serialize>(data: T, memory: &Memory, ptr: usize, len: usize){
-    let serialized_array = bincode::serialize(&data).expect("Failed to serialize type");
-    write_bytes_to_wasm_memory(&*serialized_array, memory, ptr, len)
+    modularis::write_bincode_to_wasm_memory(data,memory,ptr,len)
 }
 
 pub fn write_bytemuck_to_wasm_memory<T : bytemuck::Pod >(data: T, memory: &Memory, ptr: usize, len: usize){
-    let bytes = bytemuck::bytes_of(&data);
-    //println!("Writing {} bytes using Bytemuck",bytes.len());
-    write_bytes_to_wasm_memory(bytes, memory, ptr, len)
+    modularis::write_bytemuck_to_wasm_memory(data,memory,ptr,len)
 }
 
 pub fn write_bytes_to_wasm_memory(bytes: &[u8], memory: &Memory, ptr: usize, len: usize){
